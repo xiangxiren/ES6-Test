eedd92ce5a04ca941f68b1dbf5819a00e2d90747
xiongliwei <xiongliwei-2004@163.com>
2016-08-01 17:05:24 +0800

    增加时间戳转日期方法

--- ES6/ES6-参数.js
+++ ES6/ES6-参数.js
@@ -43,3 +43,64 @@ sayHello3(...people);
 
 //而在以前，如果需要传递数组当参数，我们需要使用函数的apply方法
 sayHello3.apply(null, people);
+
+/*
+ * 时间戳转天数
+ * */
+let calcDays = function (timeStamp) {
+    return parseInt(timeStamp / (60 * 60 * 24));
+}
+
+/*
+ * 天数转年数
+ *
+ * */
+var calcYer = function (days) {
+    let years = 0;
+    while (days > 365) {
+        days -= years++ % 4 === 2 ? 366 : 365;
+    }
+    return {years, days};
+}
+
+/*
+ *天数转月数
+ * */
+var calcMonth = function (year, days) {
+    let months = 0;
+    let isLeapYear = year % 4 === 0;
+    while (days > 28) {
+        months++;
+        let diff = 0;
+        switch (months) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                diff = 31;
+                break;
+            case 2:
+                diff = isLeapYear ? 29 : 28;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                diff = 30;
+                break;
+        }
+        days -= diff;
+    }
+    return {months, days};
+}
+
+let day = calcDays(1470041215);
+let {years, days} = calcYer(day);
+
+let {months, days:leaveDay} = calcMonth(1970 + years, days);
+
+console.log(`时间戳为1470037131，共计${years}年，${months}月，${leaveDay}天`);
+console.log(`1970年1月1日加上${years}年${months}月${leaveDay}天等于：${1970 + years}年${1 + months}月${leaveDay + 1}日`);
